a372f90b3186116dfc85ffecf6c0f5b59a6eed98
Rafi Romero <rromero@fb.com>
2015-02-02 12:25:16 -0500

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Rafi Romero on 2/2/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
